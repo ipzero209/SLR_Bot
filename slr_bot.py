1e482b663bd28038fca6dce61a95a82ce8c3d583
ipzero209 <npl127.0.0.1@gmail.com>
2018-03-02 14:55:36 -0800

    Completed version 1. Supports the following parameters:
    1. EmailIdList (via the sendTo option in the config file)
    2. RequestedBy
    3. PreparedBy
    
    Signed-off-by: ipzero209 <npl127.0.0.1@gmail.com>

--- slr_bot.py
+++ slr_bot.py
@@ -3,27 +3,147 @@
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 import xml.etree.ElementTree as et
+import getpass
+import argparse
+import sys
+from time import sleep
+import os
+
+
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
+def readOpts():
+    """Reads options from configuration file"""
+    opts_dict = {}
+    opts_dict['EmailIdList'] = []
+    opts_file = open('./slr.conf', 'r')
+    for line in opts_file:
+        line = line.strip("\n")
+        line = line.split(":")
+        if line[0] == "sendTo":
+            opts_dict['EmailIdList'].append(line[1])
+        else:
+            opts_dict[line[0]] = line[1]
+    if len(opts_dict['EmailIdList']) > 20:
+        print "Maximum number of recipients is 20. Please remove 'sendTo' entries" \
+              " in the configuration file until this criteria is met. Exiting now."
+        exit(1)
+    return opts_dict
 
-def submitStats():
+
+def getKey(dev_IP):
+    """Retrieves an API key from the porvided firewall"""
+    user = raw_input("Enter the API user name: ")
+    passwd = getpass.getpass("Enter the API password: ")
+    key_params = {"type" : "keygen",
+              "user" : user,
+              "password" : passwd}
+    key_req = requests.get("https://{}/api/?".format(dev_IP), params=key_params, verify=False)
+    key_xml = et.fromstring(key_req.content)
+    api_key = key_xml.find('./result/key').text
+    return api_key
+
+
+def genStats(dev_ip, panos_key):
+    """Issues call to the firewall to generate a stats dump"""
+    stat_params = {"type" : "export",
+                   "category" : "stats-dump",
+                   "key" : panos_key}
+    stats_req = requests.get("https://{}/api/?".format(dev_ip), params=stat_params, verify=False)
+    if stats_req.status_code != 200:
+        print "Request to generate stats dump failed. Exiting now."
+        exit(1)
+    stats_xml = et.fromstring(stats_req.content)
+    job_id = stats_xml.find('./result/job').text
+    job_result = jobChecker(dev_ip, panos_key, job_id)
+    if job_result != "OK":
+        print "There was an issue with generating the statsdump. Please check " \
+              "the device."
+    return job_id
+
+
+def jobChecker(dev_ip, panos_key, job_id):
+    """Checks the status of the job that is passed in."""
+    cmd = "<show><jobs><id>{}</id></jobs></show>".format(job_id)
+    status_params = {"type" : "op",
+                  "cmd" : cmd,
+                  "key" : panos_key}
+    status = ""
+    while status != "FIN":
+        status_req = requests.get("https://{}/api/?".format(dev_ip), params=status_params, verify=False)
+        if status_req.status_code != 200:
+            print "Request to check job status failed. Please log into device and " \
+                  "download the stats dump manually. Exiting now."
+            exit(1)
+        status_xml = et.fromstring(status_req.content)
+        status = status_xml.find('./result/job/status').text
+        result = status_xml.find('./result/job/result').text
+        if result == "PEND":
+            progress = status_xml.find('./result/job/progress').text
+            print "Stats dump job progress: {}%".format(progress)
+        sleep(5)
+    return result
+
+
+def downloadStats(dev_ip, panos_key, job_id):
+    """Downloads the statsdump file"""
+    stats_file = "stats_dump.tar.gz"
+    dl_params = {"type" : "export",
+                 "category" : "stats-dump",
+                 "action" : "get",
+                 "job-id" : job_id,
+                 "key" : panos_key}
+    dl_req = requests.get("https://{}/api/?".format(dev_ip), params=dl_params, stream=True, verify=False)
+    with open(stats_file, 'wb') as f:
+        for chunk in dl_req.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+    return stats_file
+
+
+def submitStats(file_name, opts):
     """Test function to submit stats dump for SLR generation"""
-    file1 = open('20180222_1307_statsdump.tar.gz', 'r')
+    file1 = open(file_name, 'r')
     url = "http://riskreporttest.paloaltonetworks.com/API/v1/Create"
-    key = "6a85f78e5cd9a7eccae9333361f3cbd798ee1e8c70bad9dfeb027f345e562d2d"
-    mailIDs = ['cstancill@paloaltonetworks.com']
-    requestor = "angupta@paloaltonetworks.com"
-    headers = {"apiKey" : key}
-    file = {"file1":('20180222_1307_statsdump.tar.gz', file1, 'application/gzip')}
-    payload = {"EmailIdList" : mailIDs[0],
-               "RequestedBy" : requestor}
+    headers = {"apiKey" : opts['cspKey']}
+    file = {"file1":('stats_dump.tar.gz', file1, 'application/gzip')}
+    payload = {"EmailIdList" : ",".join(opts['EmailIdList']),
+               "RequestedBy" : opts["RequestedBy"],
+               "PreparedBy" : opts['PreparedBy']}
     slr_req = requests.post(url, headers=headers, data=payload, files=file)
-    print slr_req.status_code
+    if slr_req.status_code != 200:
+        print "There was an issue submitting the stats dump:\n".format(slr_req.content)
+        exit(1)
     print slr_req.content
+    print "Cleaning up stats dump."
+    os.remove(file_name)
+
+
+
+
+
+
+
+
+
 
 def main():
-    submitStats()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--firewall", help="IP address of the firewall to pull the stats dump from", type=str)
+    args = parser.parse_args()
+    if len(sys.argv) < 2:
+        parser.print_help()
+        parser.exit()
+        exit(1)
+    fw_ip = args.firewall
+    options = readOpts()
+    key = getKey(fw_ip)
+    job_key = genStats(fw_ip, key)
+    stats_file = downloadStats(fw_ip, key, job_key)
+    submitStats(stats_file, options)
+
+
 
 
 if __name__ == '__main__':
